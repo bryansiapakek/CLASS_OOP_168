f1a642f4ebc855541353bdb754a6659a51b93bf6
bryan erdanto <bryanputra455@gmail.com>
2023-04-10 14:44:55 +0700

    Add project files.

---
+++ CLASS_OOP_168/CLASS_OOP_168.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    void tampil() {
+        cout << "NIM= " << nim;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
