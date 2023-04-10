eed76b3f9b59011482ed90004be3df9ea8e07be4
bryan erdanto <bryanputra455@gmail.com>
2023-04-10 14:45:44 +0700

    membuat kelas

--- CLASS_OOP_168/CLASS_OOP_168.cpp
+++ CLASS_OOP_168/CLASS_OOP_168.cpp
@@ -2,13 +2,3 @@
 using namespace std;
 
 class Mahasiswa {
-public:
-    int nim;
-    string nama;
-    void tampil() {
-        cout << "NIM= " << nim;
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
